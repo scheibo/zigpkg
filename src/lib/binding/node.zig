2f210f41730cf8343e66e10960eb75e967a16df0
Kirk Scheibelhut <kjs@scheibo.com>
2023-02-05 20:16:53 -0800

    fancy napi

--- src/lib/binding/node.zig
+++ src/lib/binding/node.zig
@@ -9,11 +9,11 @@ const c = @cImport({
 export fn napi_register_module_v1(env: c.napi_env, exports: c.napi_value) c.napi_value {
     const opts = options(env, "options") catch return null;
     const properties = [_]c.napi_property_descriptor{
-        prop("add", .{ .Method = add }),
-        prop("options", .{ .Value = opts }),
+        Property.init("add", .{ .method = add }),
+        Property.init("options", .{ .value = opts }),
     };
     if (c.napi_define_properties(env, exports, properties.len, &properties) != c.napi_ok) {
-        throw(.Error, env, "Failed to set exports") catch return null;
+        Error.throw(env, "Failed to set exports") catch return null;
     }
     return exports;
 }
@@ -22,118 +22,206 @@ fn add(env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.napi_value {
     var argc: usize = 1;
     var argv: [1]c.napi_value = undefined;
     if (c.napi_get_cb_info(env, info, &argc, &argv, null, null) != c.napi_ok) {
-        throw(.Error, env, "Failed to get args") catch return null;
+        Error.throw(env, "Failed to get args") catch return null;
     }
-    if (argc != 1) throw(.TypeError, env, "add requires exactly 1 argument") catch return null;
-    const n = Read.uint8(env, argv[0], "n") catch return null;
-    return Write.uint8(env, zigpkg.add(n), "result") catch return null;
+    if (argc != 1) TypeError.throw(env, "add requires exactly 1 argument") catch return null;
+    const n = Number.get(env, argv[0], "n", u8) catch return null;
+    return Number.init(env, zigpkg.add(n), "result") catch return null;
 }
 
 fn options(env: c.napi_env, comptime name: [:0]const u8) !c.napi_value {
-    var object = try Write.object(env, name);
+    var object = try Object.init(env, name);
     const properties = [_]c.napi_property_descriptor{
-        prop("foo", .{ .Value = try Write.boolean(env, zigpkg.options.foo, "foo") }),
-        prop("bar", .{ .Value = try Write.boolean(env, zigpkg.options.bar, "bar") }),
-        prop("baz", .{ .Value = try Write.boolean(env, zigpkg.options.baz, "baz") }),
-        prop("qux", .{ .Value = try Write.boolean(env, zigpkg.options.qux, "qux") }),
+        Property.init("foo", .{ .value = try Boolean.init(env, zigpkg.options.foo, "foo") }),
+        Property.init("bar", .{ .value = try Boolean.init(env, zigpkg.options.bar, "bar") }),
+        Property.init("baz", .{ .value = try Boolean.init(env, zigpkg.options.baz, "baz") }),
+        Property.init("qux", .{ .value = try Boolean.init(env, zigpkg.options.qux, "qux") }),
     };
     if (c.napi_define_properties(env, object, properties.len, &properties) != c.napi_ok) {
-        return throw(.Error, env, "Failed to set fields of " ++ name);
+        return Error.throw(env, "Failed to set fields of " ++ name);
     }
     return object;
 }
 
 const Property = union(enum) {
-    Method: c.napi_callback,
-    Value: c.napi_value,
-};
+    method: c.napi_callback,
+    value: c.napi_value,
 
-fn prop(comptime name: [:0]const u8, property: Property) c.napi_property_descriptor {
-    return .{
-        .utf8name = name,
-        .name = null,
-        .method = switch (property) {
-            .Method => |m| m,
-            else => null,
-        },
-        .getter = null,
-        .setter = null,
-        .value = switch (property) {
-            .Value => |v| v,
-            else => null,
-        },
-        .attributes = c.napi_default,
-        .data = null,
-    };
-}
-
-const Kind = enum { Error, TypeError };
-const TranslationError = error{ExceptionThrown};
-
-fn throw(comptime kind: Kind, env: c.napi_env, comptime message: [:0]const u8) TranslationError {
-    var result = switch (kind) {
-        .Error => c.napi_throw_error(env, null, message),
-        .TypeError => c.napi_throw_type_error(env, null, message),
-    };
-    switch (result) {
-        c.napi_ok, c.napi_pending_exception => {},
-        else => unreachable,
-    }
-    return TranslationError.ExceptionThrown;
-}
-
-const Read = struct {
-    fn uint8(env: c.napi_env, value: c.napi_value, comptime name: [:0]const u8) !u8 {
-        return @truncate(u8, try uint32(env, value, name));
-    }
-
-    fn uint32(env: c.napi_env, value: c.napi_value, comptime name: [:0]const u8) !u32 {
-        var result: u32 = undefined;
-        switch (c.napi_get_value_uint32(env, value, &result)) {
-            c.napi_ok => {},
-            c.napi_number_expected => return throw(.Error, env, name ++ " must be a number"),
-            else => unreachable,
-        }
-        return result;
-    }
-
-    fn boolean(env: c.napi_env, value: c.napi_value, comptime name: [:0]const u8) !bool {
-        var result: u32 = undefined;
-        switch (c.napi_get_value_bool(env, value, &result)) {
-            c.napi_ok => {},
-            c.napi_boolean_expected => return throw(.Error, env, name ++ " must be a boolean"),
-            else => unreachable,
-        }
-        return result;
+    fn init(comptime name: [:0]const u8, property: Property) c.napi_property_descriptor {
+        return .{
+            .utf8name = name,
+            .name = null,
+            .method = switch (property) {
+                .method => |m| m,
+                else => null,
+            },
+            .getter = null,
+            .setter = null,
+            .value = switch (property) {
+                .value => |v| v,
+                else => null,
+            },
+            .attributes = c.napi_default,
+            .data = null,
+        };
     }
 };
 
-const Write = struct {
-    fn uint8(env: c.napi_env, value: u8, comptime name: [:0]const u8) !c.napi_value {
-        return uint32(env, @as(u32, value), name);
+const Error = struct {
+    fn throw(env: c.napi_env, comptime message: [:0]const u8) error{Exception} {
+        return switch (c.napi_throw_error(env, null, message)) {
+            c.napi_ok, c.napi_pending_exception => error.Exception,
+            else => unreachable,
+        };
     }
+};
 
-    fn uint32(env: c.napi_env, value: u32, comptime name: [:0]const u8) !c.napi_value {
+const TypeError = struct {
+    fn throw(env: c.napi_env, comptime message: [:0]const u8) error{Exception} {
+        return switch (c.napi_throw_type_error(env, null, message)) {
+            c.napi_ok, c.napi_pending_exception => error.Exception,
+            else => unreachable,
+        };
+    }
+};
+
+const Number = struct {
+    fn init(env: c.napi_env, value: anytype, comptime name: [:0]const u8) !c.napi_value {
+        const T = @TypeOf(value);
         var result: c.napi_value = undefined;
-        if (c.napi_create_uint32(env, value, &result) != c.napi_ok) {
-            return throw(.Error, env, "Failed to create number " ++ name);
+
+        switch (@typeInfo(T)) {
+            .Int => |info| switch (info.bits) {
+                0...32 => switch (info.signedness) {
+                    .signed => {
+                        if (c.napi_create_int32(env, @as(i32, value), &result) != c.napi_ok) {
+                            return Error.throw(env, "Failed to create int32 " ++ name);
+                        }
+                    },
+                    .unsigned => {
+                        if (c.napi_create_uint32(env, @as(u32, value), &result) != c.napi_ok) {
+                            return Error.throw(env, "Failed to create int32 " ++ name);
+                        }
+                    },
+                },
+                33...51 => {
+                    if (c.napi_create_int64(env, @as(i64, value), &result) != c.napi_ok) {
+                        return Error.throw(env, "Failed to create int64 " ++ name);
+                    }
+                },
+                52 => {
+                    if (c.napi_create_int64(env, @as(i64, @as(i52, value)), &result) != c.napi_ok) {
+                        return Error.throw(env, "Failed to create int64 " ++ name);
+                    }
+                },
+                else => @compileError("int can't be represented as JS number"),
+            },
+            .ComptimeInt => {
+                if (value >= std.math.minInt(i32) and value <= std.math.maxInt(i32)) {
+                    if (c.napi_create_int32(env, @as(i32, value), &result) != c.napi_ok) {
+                        return Error.throw(env, "Failed to create int32 " ++ name);
+                    }
+                } else if (value >= std.math.minInt(i52) and value <= std.math.maxInt(i52)) {
+                    if (c.napi_create_int64(env, @as(i64, @as(i52, value)), &result) != c.napi_ok) {
+                        return Error.throw(env, "Failed to create int64 " ++ name);
+                    }
+                } else {
+                    @compileError("comptime_int can't be represented as JS number");
+                }
+            },
+            .Float, .ComptimeFloat => {
+                if (c.napi_create_double(env, @floatCast(f64, value), &result) != c.napi_ok) {
+                    return Error.throw(env, "Failed to create double " ++ name);
+                }
+            },
+            else => @compileError("expected number, got: " ++ @typeName(T)),
         }
+
         return result;
     }
 
-    fn boolean(env: c.napi_env, value: bool, comptime name: [:0]const u8) !c.napi_value {
-        var result: c.napi_value = undefined;
-        if (c.napi_get_boolean(env, value, &result) != c.napi_ok) {
-            return throw(.Error, env, "Failed to create boolean " ++ name);
+    fn get(env: c.napi_env, value: c.napi_value, comptime name: [:0]const u8, comptime T: type) !T {
+        switch (@typeInfo(T)) {
+            .Float => {
+                var result: f64 = undefined;
+                return switch (c.napi_get_value_double(env, value, &result)) {
+                    c.napi_ok => @floatCast(T, result),
+                    c.napi_number_expected => Error.throw(env, name ++ " must be a number"),
+                    else => unreachable,
+                };
+            },
+            .Int => |info| switch (info.bits) {
+                0...32 => switch (info.signedness) {
+                    .signed => {
+                        var result: i32 = undefined;
+                        return switch (c.napi_get_value_int32(env, value, &result)) {
+                            c.napi_ok => if (info.bits == 32) result else @intCast(T, result),
+                            c.napi_number_expected => Error.throw(env, name ++ " must be a number"),
+                            else => unreachable,
+                        };
+                    },
+                    .unsigned => {
+                        var result: u32 = undefined;
+                        return switch (c.napi_get_value_uint32(env, value, &result)) {
+                            c.napi_ok => if (info.bits == 32) result else @intCast(T, result),
+                            c.napi_number_expected => Error.throw(env, name ++ " must be a number"),
+                            else => unreachable,
+                        };
+                    },
+                },
+                33...63 => {
+                    var result: i64 = undefined;
+                    return switch (c.napi_get_value_int64(env, value, &result)) {
+                        c.napi_ok => @intCast(T, result),
+                        c.napi_number_expected => Error.throw(env, name ++ " must be a number"),
+                        else => unreachable,
+                    };
+                },
+                else => {
+                    var result: i64 = undefined;
+                    return switch (c.napi_get_value_int64(env, value, &result)) {
+                        c.napi_ok => switch (info.signedness) {
+                            .signed => return @as(T, value),
+                            .unsigned => return if (0 <= value)
+                                @intCast(T, value)
+                            else
+                                error.Overflow,
+                        },
+                        c.napi_number_expected => Error.throw(env, name ++ " must be a number"),
+                        else => unreachable,
+                    };
+                },
+            },
+            else => @compileError("expected number, got: " ++ @typeName(T)),
         }
-        return result;
     }
+};
 
-    fn object(env: c.napi_env, comptime name: [:0]const u8) !c.napi_value {
+const Object = struct {
+    fn init(env: c.napi_env, comptime name: [:0]const u8) !c.napi_value {
         var result: c.napi_value = undefined;
         if (c.napi_create_object(env, &result) != c.napi_ok) {
-            return throw(.Error, env, "Failed to create object " ++ name);
+            return Error.throw(env, "Failed to create object " ++ name);
         }
         return result;
+    }
+};
+
+const Boolean = struct {
+    fn init(env: c.napi_env, value: bool, comptime name: [:0]const u8) !c.napi_value {
+        var result: c.napi_value = undefined;
+        if (c.napi_get_boolean(env, value, &result) != c.napi_ok) {
+            return Error.throw(env, "Failed to create boolean " ++ name);
+        }
+        return result;
+    }
+
+    fn get(env: c.napi_env, value: c.napi_value, comptime name: [:0]const u8) !bool {
+        var result: u32 = undefined;
+        return switch (c.napi_get_value_bool(env, value, &result)) {
+            c.napi_ok => result,
+            c.napi_boolean_expected => Error.throw(env, name ++ " must be a boolean"),
+            else => unreachable,
+        };
     }
 };
