a01097e33a70cf6d0faeb3279da069de51987c6d
Kirk Scheibelhut <kjs@scheibo.com>
2023-02-25 11:53:26 -0800

    make c-example match zig-example

--- src/examples/c/example.c
+++ src/examples/c/example.c
@@ -1,13 +1,24 @@
+#include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "zigpkg.h"
+#include <zigpkg.h>
 
 int main(int argc, char **argv)
 {
-   (void)argc;
-   printf("%d\n", zigpkg_add(atoi(argv[1])));
-   printf("foo=%d bar=%d baz=%d qux=%d\n",
-      ZIGPKG_OPTIONS.foo, ZIGPKG_OPTIONS.bar, ZIGPKG_OPTIONS.baz, ZIGPKG_OPTIONS.qux);
+   if (argc != 2) {
+      fprintf(stderr, "Usage: %s <num>\n", argv[0]);
+      return 1;
+   }
+
+   char *end = NULL;
+   uint64_t num = strtoul(argv[1], &end, 10);
+   if (errno || num > 255) {
+      fprintf(stderr, "Invalid seed: %s\n", argv[1]);
+      fprintf(stderr, "Usage: %s <seed>\n", argv[0]);
+      return 1;
+   }
+
+   printf("%d\n", zigpkg_add((uint8_t)num));
    return 0;
 }
