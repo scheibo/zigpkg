c9481b52063811e693b825021135c1c559bd1f21
Kirk Scheibelhut <kjs@scheibo.com>
2023-02-28 13:07:13 -0800

    wasm bindings

---
+++ src/lib/binding/wasm.zig
@@ -0,0 +1,13 @@
+const zigpkg = @import("../zigpkg.zig");
+
+export const ADD = zigpkg.options.add;
+export const SUBTRACT = zigpkg.options.subtract;
+
+extern fn overflow() void;
+
+export fn compute(n: u32) u32 {
+    return zigpkg.compute(n) catch err: {
+        overflow();
+        break :err 0;
+    };
+}
