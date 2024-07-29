f539e0a96113d240d0d06dab87ae812d19131861
Kirk Scheibelhut <kjs@scheibo.com>
2024-07-29 15:16:00 -0700

    reformat

--- examples/zig/build.zig
+++ examples/zig/build.zig
@@ -13,10 +13,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .target = target,
     });
-    const zigpkg = b.dependency("zigpkg", .{
-        .add = add,
-        .subtract = subtract,
-    });
+    const zigpkg = b.dependency("zigpkg", .{ .add = add, .subtract = subtract });
     exe.root_module.addImport("zigpkg", zigpkg.module("zigpkg"));
     b.installArtifact(exe);
 
